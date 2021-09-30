a954e0b09c939dcc9cd5f5ce988a66136abb9d84
root <root@Divel.localdomain>
2021-09-30 14:21:14 -0600

    lests encript

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(credia.hn)
+domains=(www.credia.hn credia.hn)
 rsa_key_size=4096
 data_path="./certbot"
 email="" # Adding a valid address is strongly recommended
