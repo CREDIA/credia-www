2b0c93af8c6cfde3baafbb53a12772fb6089a916
root <root@Divel.localdomain>
2021-10-01 18:56:25 -0600

    init

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -73,7 +73,7 @@ docker-compose run --rm --entrypoint "\
     $domain_args \
     --rsa-key-size $rsa_key_size \
     --agree-tos \
-    --force-renewal" certbot renew --dry-run
+    --force-renewal" certbot --dry-run
 echo
 
 echo "### Reloading nginx ..."
